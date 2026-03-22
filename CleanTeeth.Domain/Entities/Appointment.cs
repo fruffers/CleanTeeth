b28743749c0fb446345bc743d4a3d5732a54ce11
Louise Bilous <bilouslouise@gmail.com>
2026-03-22 21:07:13 +0000

    Initial.

---
+++ CleanTeeth.Domain/Entities/Appointment.cs
@@ -0,0 +1,66 @@
+﻿using CleanTeeth.Domain.Enums;
+using CleanTeeth.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Domain.Entities
+{
+    public class Appointment
+    {
+        public Guid Id { get; private set; }
+        public Guid PatientId { get; private set; }
+        public Guid DentistId { get; private set; }
+        public Guid DentalOfficeId { get; private set; }
+        public AppointmentStatus Status { get; private set; }
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public Patient? Patient { get; private set; }
+        public Dentist? Dentist { get; private set; }
+        public DentalOffice? DentalOffice { get; private set; }
+
+        public Appointment(Guid patientId, Guid dentistId, Guid dentalOfficeId, DateTime startTime, 
+            DateTime endTime)
+        {
+            if (startTime > endTime)
+            {
+                throw new BusinessRuleException("The start time cannot be after the end time of the appointment.");
+            }
+
+            if (startTime < DateTime.Now)
+            {
+                               throw new BusinessRuleException("The start time cannot be in the past.");
+            }
+
+            PatientId = patientId;
+            DentistId = dentistId;
+            DentalOfficeId = dentalOfficeId;
+            StartTime = startTime;
+            EndTime = endTime;
+            Status = AppointmentStatus.Scheduled;
+            Id = Guid.NewGuid();
+        }
+
+        public void Cancel()
+        {
+            if (Status != AppointmentStatus.Scheduled)
+            {
+                throw new BusinessRuleException("Only scheduled appointments can be cancelled.");
+            }
+
+            Status = AppointmentStatus.Cancelled;
+        }
+
+        public void Complete()
+        {
+            if (Status != AppointmentStatus.Scheduled)
+            {
+                throw new BusinessRuleException("Only scheduled appointments can be completed.");
+            }
+
+            Status = AppointmentStatus.Completed;
+        }
+    }
+}
---
+++ CleanTeeth.Domain/Entities/Patient.cs
@@ -0,0 +1,38 @@
+﻿using CleanTeeth.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Domain.Entities
+{
+    public class Patient
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = null!;
+        public string Email { get; private set; } = null!;
+
+        public Patient(string name, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new BusinessRuleException($"The {nameof(name)} is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new BusinessRuleException($"The {nameof(email)} is required.");
+            }
+
+            if (!email.Contains("@"))
+            {
+                throw new BusinessRuleException($"The {nameof(email)} is invalid.");
+            }
+
+            Name = name;
+            Email = email;
+            Id = Guid.NewGuid();
+        }
+    }
+}
