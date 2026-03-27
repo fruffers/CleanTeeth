23d24fb0fd96b0af3aa2afb5a5d9676d174de72c
Louise Bilous <bilouslouise@gmail.com>
2026-03-27 09:06:48 +0000

    Add unit tests.

---
+++ CleanTeeth.Tests/Domain/Entities/PatientTests.cs
@@ -0,0 +1,36 @@
+﻿using CleanTeeth.Domain.Entities;
+using CleanTeeth.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanTeeth.Tests.Domain.Entities
+{
+    [TestFixture]
+    public class PatientTests
+    {
+        [Test]
+        public void Constructor_NullName_ThrowsBusinessRuleException()
+        {
+            Assert.Throws<BusinessRuleException>(() => new Patient(null!, new CleanTeeth.Domain.ValueObjects.Email("validemail@gmail.com")));
+        }
+
+        [Test]
+        public void Constructor_NullEmail_ThrowsBusinessRuleException()
+        {
+            Assert.Throws<BusinessRuleException>(() => new Patient("John Doe", null!));
+        }
+
+        [Test]
+        public void Constructor_ValidParameters_CreatesPatient()
+        {
+            // Arrange
+            string name = "John Doe";
+            CleanTeeth.Domain.ValueObjects.Email email = new CleanTeeth.Domain.ValueObjects.Email("validemail@gmail.com");
+            // Act and assert
+            Assert.DoesNotThrow(() => new Patient(name, email));
+        }
+    }
+}
---
+++ CleanTeeth.Tests/Domain/ValueObjects/EmailTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using CleanTeeth.Domain.Exceptions;
+using CleanTeeth.Domain.ValueObjects;
+
+namespace CleanTeeth.Tests.Domain.ValueObjects
+{
+    [TestFixture]
+    public class EmailTests
+    {
+        [Test]
+        public void Constructor_NullEmail_ThrowsBusinessRuleException()
+        {
+            Assert.Throws<BusinessRuleException>(() => new Email(null!));
+        }
+
+        [Test]
+        public void Constructor_EmailWithoutAtSymbol_ThrowsBusinessRuleException()
+        {
+            Assert.Throws<BusinessRuleException>(() => new Email("invalidemail.com"));
+        }
+
+        [Test]
+        public void Constructor_ValidEmail_NoExceptionsThrown()
+        {
+            Assert.DoesNotThrow(() => new Email("validemail@example.com"));
+        }
+    }
+}
